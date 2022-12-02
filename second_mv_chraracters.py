a337f86dd9fc23012c88765343a31217b14d8ed6
Huntez <osigor5464@gmail.com>
2022-12-02 09:45:54 +0200

    fixing page parsing

--- second_mv_chraracters.py
+++ second_mv_chraracters.py
@@ -1,30 +1,32 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from time import sleep
 import links
+import time
 
 driver_path = Service(links.cdrive_path)
 driver = webdriver.Chrome(service=driver_path)
 
 try:
+    file = open("marvel_characters.txt", "w")
+    href_list, chrc_list = [], []
     driver.get(links.mv_characters)
     next_button = driver.find_element(By.XPATH, links.next_button)
-    file = open("marvel_characters.txt", "w")
-    for i in range(1):
+    for page_number in range(2):
+        time.sleep(5)
         character_names = driver.find_elements(By.XPATH, links.chr_name)
         character_links = driver.find_elements(By.XPATH, links.chr_links)
-        href_list = [i.get_attribute("href") for i in character_links]
-        chrc_list = [i.text for i in character_names]
-        for j in range(len(href_list)):
-            driver.get(href_list[j])
-            category = driver.find_elements(By.XPATH, links.bio_ctg)
-            info = driver.find_elements(By.XPATH, links.bio_info)
-            file.write("\n" + chrc_list[j] + " - " + href_list[j] + "\n")
-            for ctg, inf in zip(category, info):
-                file.write(ctg.text + " - " + inf.text + "\n")
-        driver.get(links.mv_characters)
+        href_list += [i.get_attribute("href") for i in character_links]
+        chrc_list += [i.text for i in character_names]
+        print(chrc_list)
         next_button.click()
+    for link_number in range(len(href_list)):
+        driver.get(href_list[link_number])
+        category = driver.find_elements(By.XPATH, links.bio_ctg)
+        info = driver.find_elements(By.XPATH, links.bio_info)
+        file.write("\n" + chrc_list[link_number] + " - " + href_list[link_number] + "\n")
+        for ctg, inf in zip(category, info):
+            file.write(ctg.text + " - " + inf.text + "\n")
     file.close()
 except Exception as error:
     print(f"Error - {error}")
